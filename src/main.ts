8e9e39105bc574dba78eeb6e9df7a651d4d1b2df
dangngockhieu <dangngockhieu28082005@gmail.com>
2026-04-23 07:41:46 +0700

    add helmet and cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 import cookieParser from 'cookie-parser';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,12 +12,19 @@ async function bootstrap() {
 
   app.use(cookieParser());
 
+  const origins = process.env.CORS_ORIGINS?.split(',').map(s => s.trim());
+  app.enableCors({
+    origin: origins,
+    credentials: true
+  });
+
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true, // Loại bỏ các thuộc tính không được định nghĩa trong DTO
     forbidNonWhitelisted: true, // Ném lỗi nếu có thuộc tính không được định nghĩa trong DTO
     transform: true, // Tự động chuyển đổi payload thành instance của DTO
   }));
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+  app.use(helmet());
   app.setGlobalPrefix('api/v1');
   await app.listen(configService.get<string>('PORT') ?? 3000);
   if (module.hot) {
